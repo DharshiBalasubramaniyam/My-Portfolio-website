67b1dbd8d7a244fd412a1ef1bb16793b3a3dd4f6
dharshib <Dharshib.8@gmail.com>
2024-03-15 09:50:55 +0530

    update contact form

---
+++ assets/script/contactForm.js
@@ -0,0 +1,45 @@
+const contactForm = document.getElementById("contact-form")
+const name = document.getElementById("name")
+const email = document.getElementById("email")
+const subject = document.getElementById("subject")
+const body = document.getElementById("body")
+const error = document.getElementById("error")
+
+contactForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    if (validateFields()) {
+        sendMail()
+    }
+})
+
+function validateFields() {
+    let error_message = ""
+    if (name.value.trim() === "") {
+        error_message = "Your name is required!"
+    }
+    else if (email.value.trim() === "") {
+        error_message = "Your email is required!"
+    }
+    else if (subject.value.trim() === "") {
+        error_message = "Subject is required!"
+    }
+    else if (body.value.trim() === "") {
+        error_message = "Message is required!"
+    }
+    error.textContent = error_message
+    return error_message === "";
+}
+
+function sendMail() {
+    const sender_name = name.value.trim();
+    const sender_email = email.value.trim();
+    const mail_subject = subject.value.trim();
+    const mail_body = body.value.trim();
+
+    const mail_link = "mailto:dharshib.8@gmail.com" +
+        "?Subject=" + encodeURIComponent(mail_subject) +
+        "&body=Hi Dharshi, \nI am " + sender_name + "found through your portfolio." + encodeURIComponent(mail_body)
+
+    window.location.href = mail_link;
+}
+
